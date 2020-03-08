824922a3b147b31b66277699708c14888eff3159
hyogeon <men1219@hanmail.net>
2020-03-08 23:47:46 +0900

    refactor: TestSuite by Reflection

--- src/xunit/TestSuite.java
+++ src/xunit/TestSuite.java
@@ -1,10 +1,27 @@
 package xunit;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TestSuite implements Test {
     List<Test> tests = new ArrayList<>();
+
+    public TestSuite() {
+
+    }
+
+    public TestSuite(Class<? extends TestCase> testClass) {
+        Arrays.stream(testClass.getMethods())
+                .filter(m -> m.getName().startsWith("test"))
+                .forEach(m -> {
+                    try {
+                        add(testClass.getConstructor(String.class).newInstance(m.getName()));
+                    } catch (Exception e) {
+                        throw new RuntimeException();
+                    }
+                });
+    }
 
     public void add(Test test) {
         tests.add(test);
