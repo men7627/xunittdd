4f11f884b86a68be1ab91d2e2c976fca47dd2d80
hyogeon <men1219@hanmail.net>
2020-03-08 22:29:08 +0900

    feat: running test method

---
+++ src/xunit/WasRun.java
@@ -0,0 +1,13 @@
+package xunit;
+
+public class WasRun extends TestCase {
+    public boolean wasRun = false;
+
+    public WasRun(String name) {
+        super(name);
+    }
+
+    public void testMethod() {
+        wasRun = true;
+    }
+}
